60b0daa4cedddcdaced37a4ccbda6644a47362db
kooyesung <jh8ckr@gmail.com>
2024-06-18 17:25:19 +0900

    feature/selectProductsByCode-selectDetailProducts (#3)

--- src/main/java/com/dogpamines/dogseek/products/controller/ProductsController.java
+++ src/main/java/com/dogpamines/dogseek/products/controller/ProductsController.java
@@ -1,4 +1,39 @@
 package com.dogpamines.dogseek.products.controller;
 
+import com.dogpamines.dogseek.products.model.service.ProductsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class ProductsController {
+
+    private ProductsService productsService;
+
+    @Autowired
+    public ProductsController(ProductsService productsService) {
+        this.productsService = productsService;
+    }
+
+    @GetMapping("/products/{prodCode}")
+    public ResponseEntity<Map<String, Object>> selectFindByCode(@PathVariable int prodCode) {
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(new MediaType("application", "JSON", Charset.forName("UTF-8")));
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("product", productsService.selectFindByCode(prodCode));
+
+        return new ResponseEntity<>(result, headers, HttpStatus.OK);
+    }
 }
--- src/main/java/com/dogpamines/dogseek/products/model/service/ProductsService.java
+++ src/main/java/com/dogpamines/dogseek/products/model/service/ProductsService.java
@@ -1,4 +1,20 @@
 package com.dogpamines.dogseek.products.model.service;
 
+import com.dogpamines.dogseek.products.model.dao.ProductsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductsService {
+
+    private ProductsMapper prouctsMapper;
+
+    @Autowired
+    public ProductsService(ProductsMapper productsMapper) {
+        this.prouctsMapper = productsMapper;
+    }
+
+    public Object selectFindByCode(int prodCode) {
+        return prouctsMapper.selectFindByCode(prodCode);
+    }
 }
