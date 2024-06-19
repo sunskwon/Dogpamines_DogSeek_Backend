a8ae226c62fc8d2a1a6a8d595cf1ae26f0f7b303
박진현 <emem2080@naver.com>
2024-06-19 17:26:29 +0900

    [FEATURE][BOARD] 공지 및 게시글 전체 조회, 게시글 상세 조회 쿼리문 수정해야함

--- src/main/java/com/dogpamines/dogseek/board/model/dto/BoardDTO.java
+++ src/main/java/com/dogpamines/dogseek/board/model/dto/BoardDTO.java
@@ -1,4 +1,106 @@
 package com.dogpamines.dogseek.board.model.dto;
 
+import java.util.Date;
+
 public class BoardDTO {
+
+    private int postCode;
+    private String postTitle;
+    private String postContext;
+    private Date postDate;
+    private String postCategory;
+    private String postStatus;
+    private int userCode;
+    private String userNick;
+
+    public BoardDTO() {}
+
+    public BoardDTO(int postCode, String postTitle, String postContext, Date postDate, String postCategory, String postStatus, int userCode, String userNick) {
+        this.postCode = postCode;
+        this.postTitle = postTitle;
+        this.postContext = postContext;
+        this.postDate = postDate;
+        this.postCategory = postCategory;
+        this.postStatus = postStatus;
+        this.userCode = userCode;
+        this.userNick = userNick;
+    }
+
+    public int getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(int postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public void setPostTitle(String postTitle) {
+        this.postTitle = postTitle;
+    }
+
+    public String getPostContext() {
+        return postContext;
+    }
+
+    public void setPostContext(String postContext) {
+        this.postContext = postContext;
+    }
+
+    public Date getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(Date postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getPostCategory() {
+        return postCategory;
+    }
+
+    public void setPostCategory(String postCategory) {
+        this.postCategory = postCategory;
+    }
+
+    public String getPostStatus() {
+        return postStatus;
+    }
+
+    public void setPostStatus(String postStatus) {
+        this.postStatus = postStatus;
+    }
+
+    public int getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(int userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserNick() {
+        return userNick;
+    }
+
+    public void setUserNick(String userNick) {
+        this.userNick = userNick;
+    }
+
+    @Override
+    public String toString() {
+        return "BoardDTO{" +
+                "postCode=" + postCode +
+                ", postTitle='" + postTitle + '\'' +
+                ", postContext='" + postContext + '\'' +
+                ", postDate=" + postDate +
+                ", postCategory='" + postCategory + '\'' +
+                ", postStatus='" + postStatus + '\'' +
+                ", userCode=" + userCode +
+                ", userNick='" + userNick + '\'' +
+                '}';
+    }
 }
