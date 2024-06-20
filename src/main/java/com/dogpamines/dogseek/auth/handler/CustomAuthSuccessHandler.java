5bb3681c4354b099a7274a95e8565dc92d55d735
KANGMINSEO0 <juhee0629032@gmail.com>
2024-06-20 10:49:36 +0900

    develop security 내용 주석

--- src/main/java/com/dogpamines/dogseek/auth/handler/CustomAuthSuccessHandler.java
+++ src/main/java/com/dogpamines/dogseek/auth/handler/CustomAuthSuccessHandler.java
@@ -8,39 +8,41 @@ import com.dogpamines.dogseek.user.model.dto.UserDTO;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.json.simple.JSONObject;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+//import org.json.simple.JSONObject;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
 
-public class CustomAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+public class CustomAuthSuccessHandler {}
 
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
-
-        UserDTO user = ((DetailsUser) authentication.getPrincipal()).getUser();
-
-        JSONObject jsonValue = (JSONObject) ConvertUtil.convertObjectToJsonObject(user);
-        HashMap<String, Object> responseMap = new HashMap<>();  // 반환값 설정을 위해 생성
-
-        JSONObject jsonObject;
-
-        String token = TokenUtils.generateJwtToken(user);
-        responseMap.put("userInfo", jsonValue);
-        responseMap.put("message", "로그인 성공입니다.");
-
-        response.addHeader(AuthConstants.AUTH_HEADER, AuthConstants.TOKEN_TYPE + " " + token);
-
-        jsonObject = new JSONObject(responseMap);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter printWriter = response.getWriter();
-        printWriter.print(jsonObject);
-        printWriter.flush();
-        printWriter.close();
-
-    }
-}
+//public class CustomAuthSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+//
+//    @Override
+//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
+//
+//        UserDTO user = ((DetailsUser) authentication.getPrincipal()).getUser();
+//
+//        JSONObject jsonValue = (JSONObject) ConvertUtil.convertObjectToJsonObject(user);
+//        HashMap<String, Object> responseMap = new HashMap<>();  // 반환값 설정을 위해 생성
+//
+//        JSONObject jsonObject;
+//
+//        String token = TokenUtils.generateJwtToken(user);
+//        responseMap.put("userInfo", jsonValue);
+//        responseMap.put("message", "로그인 성공입니다.");
+//
+//        response.addHeader(AuthConstants.AUTH_HEADER, AuthConstants.TOKEN_TYPE + " " + token);
+//
+//        jsonObject = new JSONObject(responseMap);
+//        response.setContentType("application/json");
+//        response.setCharacterEncoding("UTF-8");
+//        PrintWriter printWriter = response.getWriter();
+//        printWriter.print(jsonObject);
+//        printWriter.flush();
+//        printWriter.close();
+//
+//    }
+//}
--- src/main/java/com/dogpamines/dogseek/user/model/service/UserService.java
+++ src/main/java/com/dogpamines/dogseek/user/model/service/UserService.java
@@ -4,7 +4,7 @@ import com.dogpamines.dogseek.common.UserRole;
 import com.dogpamines.dogseek.user.model.dao.UserMapper;
 import com.dogpamines.dogseek.user.model.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
@@ -15,17 +15,17 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    public UserDTO findByUsername(String username) {
-
-        UserDTO login = userMapper.findByUsername(username);
-
-        if (!Objects.isNull(login)) {
-            return login;
-        } else {
-            return null;
-        }
-    }
+//    @Autowired
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    public UserDTO findByUsername(String username) {
+//
+//        UserDTO login = userMapper.findByUsername(username);
+//
+//        if (!Objects.isNull(login)) {
+//            return login;
+//        } else {
+//            return null;
+//        }
+//    }
 }
