cced275acdd3fb83bfa0e4bb3aaddab161cd0a2f
SOOBIN\tifkr <soobinnunu1101@gmail.com>
2024-06-28 16:44:40 +0900

    [#81] Latest prod list

--- src/main/java/com/dogpamines/dogseek/main/model/dao/MainMapper.java
+++ src/main/java/com/dogpamines/dogseek/main/model/dao/MainMapper.java
@@ -1,7 +1,12 @@
 package com.dogpamines.dogseek.main.model.dao;
 
+import com.dogpamines.dogseek.products.model.dto.ProductsDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface MainMapper {
+    List<ProductsDTO> selectLastProds();
+
 }
--- src/main/java/com/dogpamines/dogseek/main/model/service/MainService.java
+++ src/main/java/com/dogpamines/dogseek/main/model/service/MainService.java
@@ -1,4 +1,24 @@
 package com.dogpamines.dogseek.main.model.service;
 
+import com.dogpamines.dogseek.main.model.dao.MainMapper;
+import com.dogpamines.dogseek.products.model.dto.ProductsDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class MainService {
+
+    private MainMapper mainMapper;
+
+    @Autowired
+    public MainService(MainMapper mainMapper) {
+        this.mainMapper = mainMapper;
+    }
+
+    public List<ProductsDTO> selectLastProds() {
+
+        return mainMapper.selectLastProds();
+    }
+
 }
