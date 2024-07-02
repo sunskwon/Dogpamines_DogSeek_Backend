f4c29ef714c0451631fef6c66e6d65d933680494
sunskwon <157683193+sunskwon@users.noreply.github.com>
2024-07-02 09:35:55 +0900

    fix(develop)

--- src/main/java/com/dogpamines/dogseek/dict/controller/DictController.java
+++ src/main/java/com/dogpamines/dogseek/dict/controller/DictController.java
@@ -41,31 +41,31 @@ public class DictController {
         return new ResponseEntity<>(result, headers, HttpStatus.OK);
     }
 
-//    @GetMapping("/dict/{dogName}")
-//    public ResponseEntity<Map<String, Object>> selectByName(@PathVariable String dogName) {
-//
-//        HttpHeaders headers = new HttpHeaders();
-//
-//        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-//
-//        Map<String, Object> result = new HashMap<>();
-//        result.put("dict", dictService.selectByName(dogName));
-//
-//        return new ResponseEntity<>(result, headers, HttpStatus.OK);
-//    }
+   @GetMapping("/dict/{dogName}")
+   public ResponseEntity<Map<String, Object>> selectByName(@PathVariable String dogName) {
 
-    @GetMapping("/dict/{dogCode}")
-    public ResponseEntity<Map<String, Object>> selectByCode(@PathVariable int dogCode) {
+       HttpHeaders headers = new HttpHeaders();
 
-        HttpHeaders headers = new HttpHeaders();
+       headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
-        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+       Map<String, Object> result = new HashMap<>();
+       result.put("dict", dictService.selectByName(dogName));
 
-        Map<String, Object> result = new HashMap<>();
-        result.put("dict", dictService.selectByCode(dogCode));
+       return new ResponseEntity<>(result, headers, HttpStatus.OK);
+   }
 
-        return new ResponseEntity<>(result, headers, HttpStatus.OK);
-    }
+    // @GetMapping("/dict/{dogCode}")
+    // public ResponseEntity<Map<String, Object>> selectByCode(@PathVariable int dogCode) {
+
+    //     HttpHeaders headers = new HttpHeaders();
+
+    //     headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+    //     Map<String, Object> result = new HashMap<>();
+    //     result.put("dict", dictService.selectByCode(dogCode));
+
+    //     return new ResponseEntity<>(result, headers, HttpStatus.OK);
+    // }
 
     @GetMapping("/dict/search")
     public ResponseEntity<Map<String, Object>> searchDog(@RequestParam("dogName") String dogName) {
