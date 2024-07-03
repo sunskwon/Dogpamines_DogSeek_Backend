45dcbac77dadf8fc85cbc91591a184f93ba42647
eunseongjo <tkakfkekdk4@gmail.com>
2024-07-03 17:59:09 +0900

    animalRegist commit

--- src/main/java/com/dogpamines/dogseek/animalRegist/controller/AnimalRegistController.java
+++ src/main/java/com/dogpamines/dogseek/animalRegist/controller/AnimalRegistController.java
@@ -17,13 +17,26 @@ public class AnimalRegistController {
     public String registNumber () throws IOException {
 
         final String key = "cWQLIXqRtljq7xBRrn2LYC5eOfV3jhvQ%2BtZpUe5hXov%2B8TQcHIB7CoQIpcvo5mG5i9kL6wrWOJO5DMT8KsILKg%3D%3D"; //보안을 위하여 yml로 이동 필요
+        String dogRegNo = "410000001513331";
+        String rfidCd = "";
+        String ownerNm = "홍길동";
+        String ownerBirth = "";
 
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1543061/animalInfoSrvc/animalInfo"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + key); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("dog_reg_no","UTF-8") + "=" + URLEncoder.encode("410000001513331", "UTF-8")); /*동물등록번호 또는 RFID코드 필수*/
-        urlBuilder.append("&" + URLEncoder.encode("rfid_cd","UTF-8") + "=" + URLEncoder.encode(" ", "UTF-8")); /*동물등록번호 또는 RFID코드 필수*/
-        urlBuilder.append("&" + URLEncoder.encode("owner_nm","UTF-8") + "=" + URLEncoder.encode("홍길동", "UTF-8")); /*소유자 성명 또는 생년월일 필수*/
-        urlBuilder.append("&" + URLEncoder.encode("owner_birth","UTF-8") + "=" + URLEncoder.encode(" ", "UTF-8")); /*소유자 성명 또는 생년월일 필수*/
+
+        if(!dogRegNo.isEmpty()) {
+        urlBuilder.append("&" + URLEncoder.encode("dog_reg_no","UTF-8") + "=" + URLEncoder.encode(dogRegNo, "UTF-8")); /*동물등록번호 또는 RFID코드 필수*/
+        }
+        if(!rfidCd.isEmpty()){
+        urlBuilder.append("&" + URLEncoder.encode("rfid_cd","UTF-8") + "=" + URLEncoder.encode(rfidCd, "UTF-8")); /*동물등록번호 또는 RFID코드 필수*/
+        }
+        if(!ownerNm.isEmpty()){
+        urlBuilder.append("&" + URLEncoder.encode("owner_nm","UTF-8") + "=" + URLEncoder.encode(ownerNm, "UTF-8")); /*소유자 성명 또는 생년월일 필수*/
+        }
+        if(!ownerBirth.isEmpty()){
+        urlBuilder.append("&" + URLEncoder.encode("owner_birth","UTF-8") + "=" + URLEncoder.encode(ownerBirth, "UTF-8")); /*소유자 성명 또는 생년월일 필수*/
+        }
         urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*xml(기본값) 또는 json*/
 
         URL url = new URL(urlBuilder.toString());
